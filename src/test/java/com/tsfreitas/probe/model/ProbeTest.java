855e1d4c9ef155a5f8ebd7f78955bfeb4b74bc99
THIAGO SANTOS DE FREITAS <tsfreitas@gmail.com>
2016-05-02 00:08:14 -0300

    first commit

---
+++ src/test/java/com/tsfreitas/probe/model/ProbeTest.java
@@ -0,0 +1,45 @@
+package com.tsfreitas.probe.model;
+
+import org.junit.Test;
+
+/**
+ * Created by tsfreitas on 30/04/16.
+ */
+public class ProbeTest {
+
+    @Test
+    public void deveVirarParaADireita() {
+
+    }
+
+
+    @Test
+    public void deveVirarParaAEsquerda() {
+
+    }
+
+
+    @Test
+    public void deveAndarParaONorte() {
+
+    }
+
+
+    @Test
+    public void deveAndarParaOSul() {
+
+    }
+
+    @Test
+    public void deveAndarParaOLeste() {
+
+    }
+
+
+    @Test
+    public void deveAndarParaOOeste() {
+
+    }
+
+
+}
